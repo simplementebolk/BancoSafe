be7f388306974913b72562454af184065a65d440
simplementebolk <enzoaraya.dev@gmail.com>
2026-08-13 01:02:11 -0400

    Primer commit

---
+++ src/main/java/com/banco/Banco/BancoApplication.java
@@ -0,0 +1,22 @@
+package com.banco.Banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApplication.class, args);
+	}
+
+	@GetMapping("/index")
+	public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
